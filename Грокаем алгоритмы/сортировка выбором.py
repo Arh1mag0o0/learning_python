feac922256051001754c7eeb2fecfde544e1296e
Arh1mag0o0 <ableevv.an@yandex.ru>
2023-10-20 20:15:12 +0300

    Добавил сортировка выбором

---
+++ Грокаем алгоритмы/сортировка выбором.py
@@ -0,0 +1,19 @@
+# Сортировка выбором первая функция находит наименьший элемент входного массива, вторая переписывает его в новый массив через pop и вызывает последовательно первую, вторая возвращает отсортированный массив
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([3, 1, 5, 12, 34, 32]))
