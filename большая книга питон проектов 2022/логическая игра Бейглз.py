212f873bb173079e1a1d4991ba59c8250da958ee
Arh1mag0o0 <ableevv.an@yandex.ru>
2023-12-03 18:18:09 +0300

    Дописал игру Бейглз ver 0.1

--- большая книга питон проектов 2022/логическая игра Бейглз.py
+++ большая книга питон проектов 2022/логическая игра Бейглз.py
@@ -3,8 +3,19 @@ import random
 def rand_number(len_numb):
     return ''.join(random.choices('0123456789', k=len_numb))
 
-def chek(attempt):
-    pass
+def chek(attempt, win):
+    value_good = 0
+    value_bad = 0
+    if attempt == win:
+        return [100]
+    for i in range(len(attempt)):
+        if attempt[i] == win[i]:
+            value_good += 1
+        elif attempt[i] in win:
+            value_bad += 1
+    return [value_good, value_bad]
+            
+    
 
 def main():
     print('''
@@ -14,10 +25,26 @@ def main():
           ver 0.1
           ''')
     while True:     #Основной блок
+        out = 0
         len_number = int(input('Введите длинну последовательности от 1 до 10: '))
         count_attempts = int(input('Введите количество попыток: '))
-        rand_number(len_number)
-        
+        number = rand_number(len_number)
+        for _ in range(count_attempts):
+            att = input('Вы предпологаете что загадано:')
+            value = chek(att, number)
+            if value[0] == 100:
+                out = 1
+                break
+            print('\nЧисел стоит на правильных позициях:{} \nЧисел находящихся в задуманном, но не на своих позициях:{}\n'.format(value[0], value[1]))
+        if out == 1:
+            print('Вы угадали задуманное число и это:{} \n      Поздравляем с победой!!!'.format(number))
+            print('Чтобы сыграть ещё раз введите 1, чтобы выйти введите 0')
+        else:
+            print('К сожалению вы проиграли 0_o')
+            print('Чтобы сыграть ещё раз введите 1, чтобы выйти введите 0')
+        param = int(input())
+        if param == 0:
+            break
         
         
 if __name__ == '__main__':
